625f3c9f314ede163bcf7ea50a45b55b50b2d0dc
Inprisltd <developer@inprisLtd.com>
2019-03-30 12:00:29 +0300

    update UtilsGenerator file

--- app/src/main/java/itzik/com/texiseta/utils/UtilsGenerator.java
+++ app/src/main/java/itzik/com/texiseta/utils/UtilsGenerator.java
@@ -3,7 +3,6 @@ package itzik.com.texiseta.utils;
 import android.util.Log;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -13,38 +12,19 @@ public class UtilsGenerator {
 
     public static final String TAG = UtilsGenerator.class.getSimpleName();
 
+    private static final  String[] taxisName = new String[]{"Castle","Shekem","Habima","Gordon","Azrieli","Hadera"};
+
     public static List<TaxisModel> createMockTaxisItems(){
         int min = 1,max = 100,timeRandom;
         Random r = new Random();
         ArrayList<TaxisModel> list = new ArrayList<>();
-        TaxisModel taxisModel= null;
-        timeRandom = r.nextInt(max - min + 1) + min;
-        Log.d(TAG, "createMockTaxisItems: random 1? "+ timeRandom);
-        taxisModel = new TaxisModel("Castle",timeRandom);
-        list.add(taxisModel);
-        timeRandom = r.nextInt(max - min + 1) + min;
-        Log.d(TAG, "createMockTaxisItems: random 2? "+ timeRandom);
-        taxisModel = new TaxisModel("Shekem",timeRandom);
-        list.add(taxisModel);
-        timeRandom = r.nextInt(max - min + 1) + min;
-        Log.d(TAG, "createMockTaxisItems: random 2? "+ timeRandom);
-        taxisModel = new TaxisModel("Habima",timeRandom);
-        list.add(taxisModel);
-        timeRandom = r.nextInt(max - min + 1) + min;
-        Log.d(TAG, "createMockTaxisItems: random 3? "+ timeRandom);
-        taxisModel = new TaxisModel("Gordon",timeRandom);
-        list.add(taxisModel);
-        timeRandom = r.nextInt(max - min + 1) + min;
-        Log.d(TAG, "createMockTaxisItems: random 4? "+ timeRandom);
-        taxisModel = new TaxisModel("Azrieli",timeRandom);
-        list.add(taxisModel);
-        timeRandom = r.nextInt(max - min + 1) + min;
-        Log.d(TAG, "createMockTaxisItems: random 5? "+ timeRandom);
-        taxisModel = new TaxisModel("Hadera",timeRandom);
-        list.add(taxisModel);
+        for(int i = 0 ; i<taxisName.length ; i++){
+            timeRandom = r.nextInt(max - min + 1) + min;
+            TaxisModel taxisModel = new TaxisModel(taxisName[i],timeRandom);
+            Log.d(TAG, String.format("createMockTaxisItems: taxis info ?\n%s ",taxisModel));
+            list.add(taxisModel);
+        }
         return list;
-
-
     }
 
 }
